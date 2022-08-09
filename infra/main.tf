db073b98195e27e7345ab147371137a02a044bc9
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-09 11:50:58 -0300

    core

--- infra/main.tf
+++ infra/main.tf
@@ -24,9 +24,12 @@ resource "aws_ecr_repository" "main" {
   name                 = local.image_name
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
+
 }
 
 resource "aws_ecr_repository_policy" "main" {
